9ac4ce9b7b8e8b399881b8011d24ffb73abcd94d
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-04-30 16:22:52 +0330

    jalili persian date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,42 @@ import { LoginComponent } from './auth/login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { TaskComponent } from './task/task.component';
 import { HomeComponent } from './home/home.component';
-import { ReactiveFormsModule,FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
+import {
+  MaterialPersianDateAdapter,
+  PERSIAN_DATE_FORMATS,
+} from './persian-date-adapter';
 
 @NgModule({
-  declarations: [AppComponent, RegisterComponent, LoginComponent, HeaderComponent, TaskComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule,ReactiveFormsModule,FormsModule],
-  providers: [provideAnimationsAsync()],
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HeaderComponent,
+    TaskComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MaterialPersianDateAdapter,
+      deps: [MAT_DATE_LOCALE],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: PERSIAN_DATE_FORMATS },
+    provideAnimationsAsync(),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
